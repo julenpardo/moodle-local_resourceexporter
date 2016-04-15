c06d7f19838fad5e8d1526dca1ec3ef6c5b58420
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-04-15 10:57:16 +0200

    master: create classs structure
    The functions of parent abstract class "resources" are static, because for the downloader won't be necessary to have an instance of every resource to add. For it, is enough to know which type of resource has to be added, and that is by calling the son classes.

---
+++ classes/downloader/downloader.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ * @package    local_usablebackup
+ * @copyright  2016 onwards Julen Pardo & Mondragon Unibertsitatea
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_usablebackup;
+
+defined('MOODLE_INTERNAL') || die();
+
+class downloader {
+
+    protected $courseid;
+    protected $filestorage;
+
+    public function __construct($courseid) {
+        $this->courseid = $courseid;
+        $this->filestorage = get_file_storage();
+    }
+
+    protected function create_zip_file() {
+
+    }
+
+    public function create_download_link() {
+
+    }
+
+}
