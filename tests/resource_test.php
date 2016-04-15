9c9837f76cd602c1e0aadcade13e2beda1a3f846
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-04-15 20:45:24 +0200

    dev-resource: code designed tests
    A concrete class has to be defined, otherwise, the implemented method cannot be tested, because the abstract classes cannot be instantiated.

--- tests/resource_test.php
+++ tests/resource_test.php
@@ -30,6 +30,96 @@ require_once($CFG->dirroot . '/local/usablebackup/classes/resources/resource.php
 
 use local_usablebackup\resource;
 
+/**
+ * This dirty workaround is required to test the implemented methods of the abstract class.
+ */
+class concrete_resource extends resource {
+    public function add_resources_to_zip($courseid, $zipfile, $parentdirectory) {
+        null;
+    }
+
+    protected function get_db_records($courseid) {
+        null;
+    }
+
+    protected function create_section_if_dir_not_exists($parentdirectory, $sectionname) {
+        return $this->create_section_dir_if_not_exists($parentdirectory, $sectionname);
+    }
+}
+
 class local_usablebackup_resource_testcase extends advanced_testcase {
+
+    protected $resource;
+
+    protected function setUp() {
+        parent::setUp();
+        $this->resource = new concrete_resource();
+    }
+
+    protected function tearDown() {
+        parent::tearDown();
+        $this->resource = null;
+    }
+
+    /**
+     * Reflection method, to access non-public methods.
+     *
+     * @param $name
+     * @return ReflectionMethod
+     */
+    protected static function get_methods($name) {
+        $class = new ReflectionClass('local_usablebackup\resource');
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+
+        return $method;
+    }
+
+    public function test_create_section_dir_if_not_exists_emtpy_section() {
+        global $CFG;
+
+        $method = self::get_methods('create_section_dir_if_not_exists');
+
+        $parentdirectory = $CFG->dataroot;
+        $sectionname = '';
+
+        $expected = $parentdirectory;
+
+        $actual = $method->invokeArgs($this->resource, array($parentdirectory, $sectionname));
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_create_section_dir_if_not_exists_dir_not_exists() {
+        global $CFG;
+
+        $method = self::get_methods('create_section_dir_if_not_exists');
+
+        $parentdirectory = $CFG->dataroot;
+        $sectionname = 'section';
+
+        $expected = $parentdirectory . '/' . $sectionname;
+
+        $actual = $method->invokeArgs($this->resource, array($parentdirectory, $sectionname));
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_create_section_dir_if_not_exists_dir_exists() {
+        global $CFG;
+
+        $method = self::get_methods('create_section_dir_if_not_exists');
+
+        $parentdirectory = $CFG->dataroot;
+        $sectionname = 'section';
+
+        $expected = $parentdirectory . '/' . $sectionname;
+
+        mkdir($expected);
+
+        $actual = $method->invokeArgs($this->resource, array($parentdirectory, $sectionname));
+
+        $this->assertEquals($expected, $actual);
+    }
 
 }
