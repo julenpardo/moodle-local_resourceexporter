f03fbe2ad1a79879b1c08ca99a48160b103dba99
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-04-16 19:08:15 +0200

    dev-file: code test for checking that "add_Resources_to_folder" effectively creates the expected files in the folder

--- tests/file_test.php
+++ tests/file_test.php
@@ -152,4 +152,104 @@ class local_usablebackup_file_testcase extends advanced_testcase {
         }
     }
 
+
+    public function test_add_resources_to_directory() {
+        global $DB, $CFG;
+
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $course = $this->filegenerator->create_course();
+
+        $resources = array();
+        $resources[0] = new stdClass();
+        $resources[0]->name = 'Unit testing rules';
+
+        $resources[1] = new stdClass();
+        $resources[1]->name = 'Software Engineering is fun';
+
+        $generatedresources = array();
+        $filesrows = array();
+        $files = array();
+
+        foreach ($resources as $resource) {
+            $resourceandfile = $this->filegenerator->create_resource($course->id, $resource->name);
+
+            array_push($generatedresources, $resourceandfile['resource']);
+            array_push($filesrows, $resourceandfile['filerow']);
+            array_push($files, $resourceandfile['file']);
+        }
+
+        // If in the database, in the 'resource' table, the number of rows is not the same as the defined resources,
+        // something is wrong.
+        $expectedresourcecount = count($resources);
+        $actualresourcecount = $DB->count_records('resource');
+
+        $this->assertEquals($expectedresourcecount, $actualresourcecount);
+
+        // If the number of files for the course is not the same as the defined resources, something is wrong.
+        $expectedfilecount = count($resources);
+        $actualfilecount = $DB->count_records_sql("SELECT count(files.*)
+                                                    FROM {files} files
+                                                    INNER JOIN {context} context
+                                                        ON files.contextid = context.id
+                                                        AND context.contextlevel = 70
+                                                    INNER JOIN {course_modules} course_modules
+                                                        ON context.instanceid = course_modules.id
+                                                    INNER JOIN {course} course
+                                                        ON course_modules.course = course.id
+                                                    INNER JOIN {resource} resource
+                                                        ON resource.course = course.id
+                                                        AND resource.id = course_modules.instance
+                                                    INNER JOIN {course_sections} course_sections
+                                                        ON course_sections.id = course_modules.section
+
+                                                    WHERE filename <> '.'
+                                                        AND course.id = ?", array($course->id));
+
+        $this->assertEquals($expectedfilecount, $actualfilecount);
+
+        // Now, finally, we can start testing the method.
+        $parentdirectory = $CFG->dataroot . '/test_add_resources_to_directory';
+        mkdir($parentdirectory);
+        $this->file->add_resources_to_directory($course->id, $parentdirectory);
+
+        // We get the actual files of the directory, omitting '.' and '..'.
+        $actualfiles = scandir($parentdirectory);
+        unset($actualfiles[0]);
+        unset($actualfiles[1]);
+        $actualfiles = array_values($actualfiles);
+
+        // We get the information of the created files, necessary later to get their name and content.
+        $getdbrecords = self::get_method('get_db_records');
+        $expectedresources = $getdbrecords->invokeArgs($this->file, array($course->id));
+        $expectedfiles = array();
+
+        $filestorage = get_file_storage();
+        foreach ($expectedresources as $expectedresource) {
+            $file = $filestorage->get_file($expectedresource->contextid,
+                $expectedresource->component,
+                $expectedresource->filearea,
+                $expectedresource->itemid,
+                $expectedresource->filepath,
+                $expectedresource->filename);
+            array_push($expectedfiles, $file);
+        }
+
+        // If the number of files defined in database and the number of physical files is not the same, or there is not
+        // expected file, something is wrong.
+        $this->assertFalse(empty($expectedfiles));
+        $this->assertEquals(count($expectedfiles), count($actualfiles));
+
+        // We compare the names of the files retrieved from database, and the names of the scanned physical files.
+        foreach ($expectedfiles as $index => $expectedfile) {
+            $expectedfilename = $expectedfile->get_filename();
+            $actualfilename = $actualfiles[$index];
+
+            $this->assertEquals($expectedfilename, $actualfilename);
+        }
+
+        // @TODO: compare files' contents.
+    }
+
 }
