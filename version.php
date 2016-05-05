78e6ef5c8a8919d718da8f113d40fea5ecc95eef
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-05-05 11:03:37 +0200

    MOODLE_30_STABLE: update version.php for v1.0 release for Moodle 3.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version      = 2016050500;
-$plugin->release      = 'master - v1.0 release (Build 2016050500) for Moodle 2.9 and 3.0.';
-$plugin->requires     = 2015051100;
+$plugin->release      = 'v3.0.1.0 (version 1.0 for Moodle 3.0) (Build 2016050500)';
+$plugin->requires     = 2015111600;
 $plugin->component    = 'local_resourceexporter';
 $plugin->maturity     = MATURITY_STABLE;
